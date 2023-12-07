f9114aabf5e48f6fb0271c6f1a84b791e2278c4f
koreanjys <connecttojys@gmail.com>
2023-12-08 01:35:14 +0900

    fastapi study

--- todo.py
+++ todo.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
+from model import Todo
 
 todo_router = APIRouter()
 
 todo_list = []
 
 @todo_router.post("/todo")
-async def add_todo(todo: dict) -> dict:
+async def add_todo(todo: Todo) -> dict:
     todo_list.append(todo)
     return {
         "message": "Todo added successfully"
@@ -15,4 +16,15 @@ async def add_todo(todo: dict) -> dict:
 async def retrieve_todos() -> dict:
     return {
         "todos": todo_list
+    }
+
+@todo_router.get("/todo/{todo_id}")
+async def get_single_todo(todo_id: int) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return {
+                "todo": todo
+            }
+    return {
+        "message": "Todo with supplied ID doesn't exist"
     }
