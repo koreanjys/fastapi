7b196088b3b9bf51747b508d5fc01fe9ee009f40
koreanjys <connecttojys@gmail.com>
2023-12-08 18:36:58 +0900

    FastAPI CRUD learned

--- model.py
+++ model.py
@@ -3,3 +3,23 @@ from pydantic import BaseModel
 class Todo(BaseModel):
     id: int
     item: str
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "id": 1,
+                "item": "Example!"
+            }
+        }
+    }
+
+class TodoItem(BaseModel):
+    item: str
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "item": "Read the next chapter of the book."
+            }
+        }
+    }
